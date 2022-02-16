5d34a6e7c2d47f582c15f24e331ffb9822fa409d
Thomas Desmoulins <thomas.desmoulins@gmail.com>
2022-02-16 17:24:16 +0100

    models ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Destroying records..."
+Garden.destroy_all if Rails.env.development?
+
+
+puts "Creating gardens..."
+little_garden = Garden.create!(
+  name: "My Little Garden",
+  banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_1.jpg"
+)
+
+Garden.create!(
+  name: "My Other Garden",
+  banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_2.jpg"
+)
+puts "Gardens created!"
+
+puts "Creating plant..."
+Plant.create!(
+  name: "rosebush",
+  image_url: "https://www.roses-guillot.com/3910-thickbox_default/rosier-american-pillar.jpg",
+  garden: little_garden
+)
+
+Plant.create!(
+  name: "olivier",
+  image_url: "https://www.snhf.org/wp-content/uploads/2020/09/Olivier-ornemental-min.jpg",
+  garden: little_garden
+)
+puts "Plants created!"
